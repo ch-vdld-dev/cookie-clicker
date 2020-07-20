e135c122e5f77278853a9be069ea76a04420fc14
Linard Jeremy <linard.jeremy.5@gmail.com>
2020-07-20 16:51:08 +0200

    première version JS

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -1,12 +1,36 @@
 let btn = document.getElementById('CookieClick');
     let target = document.getElementById('score');
-    localStorage.getItem('caugm') === null && localStorage.setItem('caugm', 0);
-    target.innerHTML = localStorage.getItem('caugm');
+    let multi =  document.getElementById('multiplicator');
+     let x =0;
+     let y=0;
+
 
     function incLocal() {
-        let x = parseInt(localStorage.getItem('caugm'), 10) + 1;
-        localStorage.setItem('caugm', x);
-        target.innerHTML = x;
+         x+=1;
+     target.innerHTML = x;
+     y+=1;
+     
+
     }
 
     btn.addEventListener('click', incLocal);
+
+
+    function lessCredit(){
+    x = target.innerHTML - parseInt(multi.value);
+    // target.innerHTML= (parseInt(target.innerHTML) - parseInt(multi.value));
+     
+    target.innerHTML=x+y;
+    multi.value = parseInt(multi.value)*2;
+       if (x<0) {
+           target.innerHTML = 0;
+       }
+       else if (x< parseInt(multi.value)){
+        multi.disabled = true;
+           
+       }
+       else if (x>=parseInt(multi.value)){
+           multi.disabled= false;
+       }
+    };
+    multi.addEventListener('click', lessCredit);
