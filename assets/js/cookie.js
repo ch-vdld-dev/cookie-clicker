9eb3ffc1b0cd1564d976a71a690d07337f0b6b49
Linard Jeremy <linard.jeremy.5@gmail.com>
2020-07-21 14:53:43 +0200

    ajout fonction multiply

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -3,30 +3,40 @@ let btn = document.getElementById('CookieClick');
     let multi =  document.getElementById('multiplicator');
      let x =0;
 
-
     function incLocal() {
-         x+=1;
+     if (multi.value==="25") {
+    x+=1;
      target.innerHTML = x;
+     }
+     if (multi.value==="50") {
+         x+=2;
+     target.innerHTML = x;
+     }
+     if (parseInt(multi.value)>50){
+        x+=4;
+        target.innerHTML = x;
+     }
+     if (x<0 && (x<parseInt)) {
+        target.innerHTML = 0;
+     }
+     if (x< parseInt(multi.value)) {
+     multi.disabled = true;
+     }  
+     if (x>=parseInt(multi.value)){
+        multi.disabled= false;
+     }
+     
     }
 
     btn.addEventListener('click', incLocal);
 
 
     function lessCredit(){
-    x = target.innerHTML - parseInt(multi.value);
+    x = parseInt(target.innerHTML) - parseInt(multi.value);
+
     // target.innerHTML= (parseInt(target.innerHTML) - parseInt(multi.value));
-     
-    target.innerHTML=x+y;
+    target.innerHTML=x;
     multi.value = parseInt(multi.value)*2;
-       if (x<0) {
-           target.innerHTML = 0;
-       }
-       else if (x< parseInt(multi.value)){
-        multi.disabled = true;
-           
-       }
-       else if (x>=parseInt(multi.value)){
-           multi.disabled= false;
-       }
+
     };
     multi.addEventListener('click', lessCredit);
